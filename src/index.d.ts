5ff7d36f680413d30f74562b9ce4bc5f6bbcd631
sashamanko <mankooleksandr748@gmail.com>
2024-09-20 04:20:18 +0200

    update exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,7 @@ import { Colors } from "./types"
 declare const clcolorize: Colors & { createColors: (enabled?: boolean) => Colors }
 declare const clc: (string: string, style: (keyof Omit<Colors, 'isColorSupported'>)[]) => string
 
+export default clcolorize;
 export {
   clcolorize,
   clc
